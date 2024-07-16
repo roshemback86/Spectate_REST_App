63ff9eb6c48a73032091f6b5a3853b920b58fa03
Oleksii <118919492+roshemback86@users.noreply.github.com>
2024-07-16 13:49:01 +0100

    ADD create Selection endpoint

--- spectate_rest_app/main.py
+++ spectate_rest_app/main.py
@@ -4,7 +4,7 @@ import sqlite3
 import httpx
 from typing import List, Optional
 import requests
-
+from decimal import Decimal
 # from spectate_rest_app.services import get_logos
 from .schemas import Sport, Event, Selection
 # TODO: разобраться с логированием ошибок, в т.ч. по созданию записей в таблах
@@ -37,6 +37,8 @@ def get_db_connection():
 @app.post("/sport", status_code=201)
 async def create_sport(sport: Sport):
     '''Create a new sport'''
+    if not sport.name:
+        raise HTTPException(status_code=400, detail="Name is required")
     slug = sport.slug if sport.slug is not None else sport.name.lower().replace(" ", "-")
     active = sport.active if sport.active is not None else True
 
@@ -60,6 +62,9 @@ async def create_sport(sport: Sport):
 @app.post("/event", status_code=201)
 async def create_event(event: Event):
     '''Create a new event'''
+    if not event.name or not event.type or not event.sport or not event.status or not event.scheduled_start:
+        raise HTTPException(status_code=400, detail="Required fields: name, type, sport, status, scheduled_start")
+
     slug = event.slug if event.slug is not None else event.name.lower().replace(" ", "-")
     active = event.active if event.active is not None else True
     teams = [team.strip() for team in event.name.split("vs")]
@@ -85,6 +90,34 @@ async def create_event(event: Event):
         raise HTTPException(status_code=400, detail=str(e))
 
     return {"id": event_id}
+
+
+@app.post("/create_selection")
+async def create_selection(selection: Selection):
+    '''Create a new selection'''
+    if not selection.name or not selection.event or not selection.price or not selection.outcome:
+        raise HTTPException(status_code=400, detail="Required fields: name, event, price, outcome")
+
+    active = True if selection.active else False
+    selection.price = Decimal(selection.price)
+    query = """
+        INSERT INTO selection (name, event, active, price, outcome) 
+        VALUES (?, ?, ?, ?, ?)
+    """
+
+    try:
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        cursor.execute(query, (
+            selection.name, selection.event, active, float(selection.price), selection.outcome
+        ))
+        conn.commit()
+        selection_id = cursor.lastrowid
+        conn.close()
+        return {"id": selection_id}
+    except sqlite3.Error as e:
+        raise HTTPException(status_code=500, detail=f"An error occurred: {e}")
+
 
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=9000)
